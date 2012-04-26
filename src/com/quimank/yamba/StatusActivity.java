2074a3b4fd9f1b23c452d645af028e669fa6ee47
Kike Poot <kikepoot@gmail.com>
2012-04-25 22:38:06 -0500

    Added a text watcher to the status activity

--- src/com/quimank/yamba/StatusActivity.java
+++ src/com/quimank/yamba/StatusActivity.java
@@ -3,20 +3,25 @@ package com.quimank.yamba;
 import winterwell.jtwitter.Twitter;
 import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class StatusActivity extends Activity implements OnClickListener {
+public class StatusActivity extends Activity implements OnClickListener, TextWatcher {
 	private static final String TAG = "StatusActivity";
 	EditText editText;
 	Button updateButton;
 	Twitter twitter;
+	TextView textCount;
 	
     /** Called when the activity is first created. */
     @SuppressWarnings("deprecation")
@@ -26,10 +31,16 @@ public class StatusActivity extends Activity implements OnClickListener {
         setContentView(R.layout.status);
         
         //Find views
-        editText = (EditText) findViewById(R.id.editText);
-        this.updateButton = (Button) this.findViewById(R.id.buttonUpdate);
+        this.editText = (EditText) findViewById(R.id.editText);
         
+        this.updateButton = (Button) this.findViewById(R.id.buttonUpdate);        
         this.updateButton.setOnClickListener(this);
+        
+        this.textCount = (TextView) this.findViewById(R.id.textCount);
+        this.textCount.setText(Integer.toString(140));
+        this.textCount.setTextColor(Color.GREEN);
+        
+        this.editText.addTextChangedListener(this);
         
         this.twitter = new Twitter("student","password");
         twitter.setAPIRootUrl("http://yamba.marakana.com/api");
@@ -46,9 +57,9 @@ public class StatusActivity extends Activity implements OnClickListener {
 	class  PostToTwitter extends AsyncTask<String, Integer, String>{
 	
 		@Override
-		protected String doInBackground(String... arg0) {
+		protected String doInBackground(String... statuses) {
 			try{
-				winterwell.jtwitter.Status status = twitter.updateStatus(arg0[0]);
+				winterwell.jtwitter.Status status = twitter.updateStatus(statuses[0]);
 				return status.text;
 			}catch(TwitterException ex){
 				Log.e(TAG, ex.toString());
@@ -68,5 +79,26 @@ public class StatusActivity extends Activity implements OnClickListener {
 		protected void onPostExecute(String result){
 			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
 		}
+	}
+
+	public void afterTextChanged(Editable statusText) {
+		int count = 140 - statusText.length();
+		this.textCount.setText(Integer.toString(count));
+		this.textCount.setTextColor(Color.GREEN);
+		if(count < 10)
+			this.textCount.setTextColor(Color.YELLOW);
+		if(count < 0)
+			this.textCount.setTextColor(Color.RED);
+	}
+
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+		// TODO Auto-generated method stub
+		
 	}
 }
